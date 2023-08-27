f29ead9167bdddf4e57696ca5fa9a91bfd0d4970
Gracdjd <ziwenwang950@gmail.com>
2023-08-27 20:51:34 +0800

    完成列表和节点的初始化

--- portmacro.h
+++ portmacro.h
@@ -3,8 +3,9 @@
 
 #include "stdint.h"
 #include "stddef.h"
+#include "freertosconfig.h"
 
-`#define portCHAR char
+#define portCHAR char
 #define portFLOAT float
 #define portDOUBLE double
 #define portLONG long
@@ -12,4 +13,16 @@
 #define portSTACK_TYPE uint32_t
 #define portBASE_TYPE long
 
+typedef portSTACK_TYPE StackType_t;
+typedef long BaseType_t;
+typedef unsigned long UBaseType_t;
+ 
+
+#if( configUSE_16_BIT_TICKS == 1 )
+typedef uint16_t TickType_t;
+#define portMAX_DELAY ( TickType_t ) 0xffff
+#else
+typedef uint32_t TickType_t; 
+#define portMAX_DELAY ( TickType_t ) 0xffffffffUL
+#endif
 #endif
