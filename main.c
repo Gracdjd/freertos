b57ed8782dfd4520b551647d94fb007ca6bba73f
Gracdjd <ziwenwang950@gmail.com>
2023-08-28 14:47:19 +0800

    for testing

---
+++ main.c
@@ -0,0 +1,23 @@
+#include "list/list.h"
+#include <stdio.h>
+int main()
+{
+    ListItem_t node;
+    vListInitialiseItem(&node);
+    node.xItemValue = 1000;
+
+    List_t l;
+    vListInitialise(&l);
+
+    vListInsert(&l, &node);
+
+    printf("hello world\n");
+    printf("%lu\n", l.uxNumberOfItems);
+    printf("%u\n", l.pxIndex->xItemValue);
+    printf("%p\n", l.pxIndex);
+
+    printf("%p\n", l.pxIndex->pxPrevious);
+    printf("%p\n", l.pxIndex->pxNext);
+
+    return 0;
+}
